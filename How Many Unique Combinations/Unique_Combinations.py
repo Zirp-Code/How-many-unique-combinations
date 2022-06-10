d3c1677584c59a358bf3f083efbaa9851534015c
Zirp-Code <107128195+Zirp-Code@users.noreply.github.com>
2022-06-10 22:15:55 +0200

    Add files via upload

---
+++ How Many Unique Combinations/Unique_Combinations.py
@@ -0,0 +1,8 @@
+def combinations(n):
+    if type(n) == int:
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            return n * combinations(n-1)
